dcb4e4b53e7c6fcbf59a3d308aeb4da308b468da
GollapudiLavanya <lavanyagollapudi5166@gmail.com>
2021-07-04 20:32:00 +0530

    [LAVANYA]wage12

--- Employee Wage Computation/CompanyEmpWage.cs
+++ Employee Wage Computation/CompanyEmpWage.cs
@@ -4,30 +4,37 @@ using System.Text;
 
 namespace EmployeeWageComputation
 {
-    class CompanyEmpWage
+    public interface IComputeEmpWage
+    {
+        // Implemented Interface........
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+        public void computeEmpWage();
+
+    }
+    class CompanyEmpWage : IComputeEmpWage
     {
         public const int FULL_TIME = 1;
         public const int PART_TIME = 2;
         // No. of Company is given.....
         public int numberOfCompany = 0;
-        public EmployeeWage[] companyEmpWageArray;
+        public LinkedList<EmployeeWage> companyEmpWageList;
         // Object Array is Created......
         public CompanyEmpWage()
         {
-            this.companyEmpWageArray = new EmployeeWage[5];
+            this.companyEmpWageList = new LinkedList<EmployeeWage>();
         }
         // Adding Company....
         public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
-            companyEmpWageArray[this.numberOfCompany] = new EmployeeWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
-            numberOfCompany++;
+            EmployeeWage employeeWage = new EmployeeWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+            this.companyEmpWageList.AddLast(employeeWage);
         }
         public void computeEmpWage()
         {
             // Employee Wage Computation is done for each Company in the Array.......
-            for (int i = 0; i < numberOfCompany; i++)
+            foreach (EmployeeWage companyWage in this.companyEmpWageList)
             {
-                computeEmpWage(this.companyEmpWageArray[i]);
+                this.computeEmpWage(companyWage);
             }
         }
         public void computeEmpWage(EmployeeWage employeeWageObject)
