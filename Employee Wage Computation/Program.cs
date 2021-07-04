0da4018f27bd8cd8dd18702769584d2a1c81aae6
GollapudiLavanya <lavanyagollapudi5166@gmail.com>
2021-07-04 15:49:31 +0530

    [LAVANYA]wage1

---
+++ Employee Wage Computation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int employeePresent = 1;
+            Random random = new Random();
+            int empInput = random.Next(0, 2);
+            if (empInput == employeePresent)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
