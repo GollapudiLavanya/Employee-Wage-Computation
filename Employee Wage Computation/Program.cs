2039d1fd5c117b5b087ca4fa3ee831a236f333a8
GollapudiLavanya <lavanyagollapudi5166@gmail.com>
2021-07-04 15:54:40 +0530

    [LAVANYA]wage3

--- Employee Wage Computation/Program.cs
+++ Employee Wage Computation/Program.cs
@@ -10,14 +10,20 @@ namespace EmployeeWageComputation
             int emp_Wage_Per_Hour = 20;
             int emp_Wage = 0;
             int emp_Hour = 0;
-            int employeePresent = 1;
+            int employee_Present = 1;
+            int employee_Part_Time = 2;
             Random random = new Random();
-            int empInput = random.Next(0, 2);
-            if (empInput == employeePresent)
+            int empInput = random.Next(0, 3);
+            if (empInput == employee_Present)
             {
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("Employee Full Time");
                 emp_Hour = 8;
 
+            }
+            else if (empInput == employee_Part_Time)
+            {
+                Console.WriteLine("Employee Part Time");
+                emp_Hour = 4;
             }
             else
             {
